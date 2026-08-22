cc92fd2c6f457901ee750956bb8d459f63a835ca
nhattruc512-hub <nhattruc512@gmail.com>
2026-08-22 19:23:19 +0700

    Load corrected manager shift-only totals

--- saovang-manager-branch.js
+++ saovang-manager-branch.js
@@ -71,7 +71,7 @@
 
 (function(){
   const s=document.createElement('script');
-  s.src='./saovang-manager-banks.js?v=19';
+  s.src='./saovang-manager-banks.js?v=34';
   s.async=true;
   document.head.appendChild(s);
 })();
